89c6625f4ce6b259fa934f84e7fd110310734da3
Abhishek-R-Soni <abhisheksoni4195@gmail.com>
2020-07-29 15:34:46 +0530

    implemented useState()

--- src/StudentC.js
+++ src/StudentC.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// class component
 class Student extends React.Component{
     render(){
         return <h1>From Class Component</h1>
--- src/StudentF.js
+++ src/StudentF.js
@@ -1,12 +1,24 @@
-import React from 'react';
+// using useState of hook
+
+import React,  { useState } from 'react';
 
 // function component
-const Student = props => {
-    return (
+const Student = () => {
+  const roll = useState(101)
+  const [name, setName] = useState("Abhishek")
+
+  const updateData = () => {
+    setName("Dhimant")
+    roll[1](1111)
+  }
+  
+  return (
             <div>
               <h1>Welcome Again,</h1>
-              <h1>Hi,I am a Student name is {props.name}</h1>
-            </div>
+              <h1>ID is {roll[0]}</h1>
+              <h1>Hi,I am a Student name is {name}</h1>
+              <button onClick={updateData}>Click</button>
+              </div>
         )
 }
 
